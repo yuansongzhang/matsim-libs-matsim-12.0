e28eb1882552a0077d3e1140e44bb6ab2a156f62
Yuansong Zhang <306336548@qq.com>
2020-09-28 21:16:20 +0800

    Learn MutableScenario

--- matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
+++ matsim/src/main/java/org/matsim/core/scenario/MutableScenario.java
@@ -40,32 +40,35 @@ import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.vehicles.VehicleUtils;
 import org.matsim.vehicles.Vehicles;
 
-
 /**
  * @author dgrether
  * @author mrieser
  */
-public final class MutableScenario implements Scenario, Lockable {
+
+/**
+ * Yuansong Zhang
+ * learning */ 
+ public final class MutableScenario implements Scenario, Lockable {
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(MutableScenario.class);
 
-	private boolean locked = false ;
+	private boolean locked = false;
 
 	private final Map<String, Object> elements = new HashMap<>();
 
-	//mandatory attributes
+	// mandatory attributes
 	private final Config config;
 	private Network network;
 	private Population population;
 	private ActivityFacilities facilities;
 
-	//non-mandatory attributes
+	// non-mandatory attributes
 	private TransitSchedule transitSchedule = null;
 	private Lanes lanes = null;
 	private Households households;
 	private Vehicles transitVehicles;
 
-	private Vehicles vehicles ;
+	private Vehicles vehicles;
 
 	MutableScenario(Config config) {
 		this.config = config;
@@ -77,7 +80,7 @@ public final class MutableScenario implements Scenario, Lockable {
 		this.vehicles = VehicleUtils.createVehiclesContainer();
 		this.transitVehicles = VehicleUtils.createVehiclesContainer();
 		this.transitSchedule = new TransitScheduleFactoryImpl().createTransitSchedule();
-		
+
 		this.config.network().setLocked();
 	}
 
@@ -98,7 +101,8 @@ public final class MutableScenario implements Scenario, Lockable {
 
 	@Override
 	public final Config getConfig() {
-		// yy should throw an exception if null. kai, based on https://matsim.atlassian.net/browse/MATSIM-301 , may'15
+		// yy should throw an exception if null. kai, based on
+		// https://matsim.atlassian.net/browse/MATSIM-301 , may'15
 		return this.config;
 	}
 
@@ -112,14 +116,18 @@ public final class MutableScenario implements Scenario, Lockable {
 		this.population = population;
 	}
 
-	// NOTE: Thibaut is not so happy about just returning null, since someone may code something like
+	// NOTE: Thibaut is not so happy about just returning null, since someone may
+	// code something like
 	// Vehicles vehicles = scenario.getVehicles() ; // returning null
 	// ... // lots of code
 	// ... vehicles.getVehicles() ... // throwing a null pointer exception
-	// That is, in such a situation the null pointer exception is thrown much later than when the null pointer was obtained.
+	// That is, in such a situation the null pointer exception is thrown much later
+	// than when the null pointer was obtained.
 	// He rather wants to have this fail when it is called.
-	// I (kn) am a bit sceptic if it makes sense to establish such a convention when most other people use "if ... == null" use as the
-	// official dialect to check if something is there (since this would cause an exception with Thibaut's approach). kai, feb'15
+	// I (kn) am a bit sceptic if it makes sense to establish such a convention when
+	// most other people use "if ... == null" use as the
+	// official dialect to check if something is there (since this would cause an
+	// exception with Thibaut's approach). kai, feb'15
 
 	@Override
 	public final Households getHouseholds() {
@@ -141,75 +149,79 @@ public final class MutableScenario implements Scenario, Lockable {
 		return this.vehicles;
 	}
 
-
-
 	@Override
 	public final TransitSchedule getTransitSchedule() {
 		return this.transitSchedule;
 	}
 
 	@Override
-	public final void addScenarioElement(
-			final String name,
-			final Object o) {
-		// Once the "removal" operation is locked, you cannot add under the same name. kai, sep'14
-		if ( o == null ) throw new NullPointerException( name );
-		final Object former = elements.put( name , o );
-		if ( former != null ) {
-			throw new IllegalStateException( former+" is already associated with name "+name+" when adding "+o );
+	public final void addScenarioElement(final String name, final Object o) {
+		// Once the "removal" operation is locked, you cannot add under the same name.
+		// kai, sep'14
+		if (o == null)
+			throw new NullPointerException(name);
+		final Object former = elements.put(name, o);
+		if (former != null) {
+			throw new IllegalStateException(former + " is already associated with name " + name + " when adding " + o);
 		}
 	}
 
 	public final Object removeScenarioElement(final String name) {
 		testForLocked();
-		return elements.remove( name );
+		return elements.remove(name);
 	}
 
 	@Override
 	public final Object getScenarioElement(final String name) {
-		// yy should throw an exception if null. kai, based on https://matsim.atlassian.net/browse/MATSIM-301 , may'15
-		return elements.get( name );
+		// yy should throw an exception if null. kai, based on
+		// https://matsim.atlassian.net/browse/MATSIM-301 , may'15
+		return elements.get(name);
 	}
 
 	@Override
 	public final void setLocked() {
-		this.locked = true ;
+		this.locked = true;
 	}
 
 	private void testForLocked() {
-		if ( locked ) {
-			throw new RuntimeException( "Scenario is locked; too late to do this.  See comments in code.") ;
-			/* The decision is roughly as follows:
-			 * - It is ok to set network, population, etc. in the Scenario during initial demand generation.
-			 * - It is NOT ok to do this once the services is running.
-			 * - But we do not want to make a defensive copy of the whole thing at services startup.
-			 * - We also want to be able to plug alternative Scenario implementations into the services.
-			 * - But then the services only gets the "Scenario" not the "MutableScenario", so it does not have to worry about setNetwork and the like
-			 * since it does not exist in the published interface.
-			 * kai, sep'14
+		if (locked) {
+			throw new RuntimeException("Scenario is locked; too late to do this.  See comments in code.");
+			/*
+			 * The decision is roughly as follows: - It is ok to set network, population,
+			 * etc. in the Scenario during initial demand generation. - It is NOT ok to do
+			 * this once the services is running. - But we do not want to make a defensive
+			 * copy of the whole thing at services startup. - We also want to be able to
+			 * plug alternative Scenario implementations into the services. - But then the
+			 * services only gets the "Scenario" not the "MutableScenario", so it does not
+			 * have to worry about setNetwork and the like since it does not exist in the
+			 * published interface. kai, sep'14
 			 */
 		}
 	}
 
-	public final void setActivityFacilities( ActivityFacilities facilities ) {
-		testForLocked() ;
-		this.facilities = facilities ;
-	}
-	public final void setHouseholds( Households households ) {
-		testForLocked() ;
-		this.households = households ;
-	}
-	public final void setTransitSchedule( TransitSchedule schedule ) {
+	public final void setActivityFacilities(ActivityFacilities facilities) {
 		testForLocked();
-		this.transitSchedule = schedule ;
+		this.facilities = facilities;
 	}
-	public final void setTransitVehicles( Vehicles vehicles ) {
+
+	public final void setHouseholds(Households households) {
 		testForLocked();
-		this.transitVehicles = vehicles ;
+		this.households = households;
 	}
-	public final void setLanes( Lanes lanes ) {
+
+	public final void setTransitSchedule(TransitSchedule schedule) {
 		testForLocked();
-		this.lanes = lanes ;
+		this.transitSchedule = schedule;
+	}
+
+	public final void setTransitVehicles(Vehicles vehicles) {
+		testForLocked();
+		this.transitVehicles = vehicles;
+	}
+
+	public final void setLanes(Lanes lanes) {
+		testForLocked();
+		this.lanes = lanes;
 	}
 
 }
