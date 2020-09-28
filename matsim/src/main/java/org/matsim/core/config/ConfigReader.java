2e21b1967e92c8becc8ab7f4345e5973d15f00e9
Yuansong Zhang <306336548@qq.com>
2020-09-28 20:45:08 +0800

    Learn ConfigReader

--- matsim/src/main/java/org/matsim/core/config/ConfigReader.java
+++ matsim/src/main/java/org/matsim/core/config/ConfigReader.java
@@ -32,12 +32,17 @@ import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 
 /**
- * A reader for config-files of MATSim. This reader recognizes the format of the config-file and uses
- * the correct reader for the specific config-version, without manual setting.
+ * A reader for config-files of MATSim. This reader recognizes the format of the
+ * config-file and uses the correct reader for the specific config-version,
+ * without manual setting.
  *
  * @author mrieser
  */
-public final class ConfigReader extends MatsimXmlParser {
+
+ /***Yuansong Zhang
+  * learning
+ */
+ public final class ConfigReader extends MatsimXmlParser {
 
 	private final static Logger log = Logger.getLogger(ConfigReader.class);
 
@@ -52,11 +57,13 @@ public final class ConfigReader extends MatsimXmlParser {
 	/**
 	 * Creates a new reader for MATSim configuration files.
 	 *
-	 * @param config The Config-object to store the configuration settings in. 
-	 * <p></p>
-	 * As far as I can tell, pre-existing settings in config are preserved
-	 * except if they are explicitly overwritten by entries in the config file.  The MATSim4UrbanSim initialization sequence depends on this
-	 * behavior.  kn, mar'13 
+	 * @param config The Config-object to store the configuration settings in.
+	 *               <p>
+	 *               </p>
+	 *               As far as I can tell, pre-existing settings in config are
+	 *               preserved except if they are explicitly overwritten by entries
+	 *               in the config file. The MATSim4UrbanSim initialization sequence
+	 *               depends on this behavior. kn, mar'13
 	 */
 	public ConfigReader(final Config config) {
 		this.config = config;
@@ -73,18 +80,20 @@ public final class ConfigReader extends MatsimXmlParser {
 	}
 
 	/**
-	 * Parses the specified config file, and uses the given dtd file as a local copy to use as dtd
-	 * if the one specified in the config file cannot be found.
+	 * Parses the specified config file, and uses the given dtd file as a local copy
+	 * to use as dtd if the one specified in the config file cannot be found.
 	 *
-	 * @param filename The name of the file to parse.
-	 * @param dtdFilename The name of a (local) dtd-file to be used for validating the config file.
+	 * @param filename    The name of the file to parse.
+	 * @param dtdFilename The name of a (local) dtd-file to be used for validating
+	 *                    the config file.
 	 * @throws UncheckedIOException e.g. if the file cannot be found
 	 */
 	public void readFile(final String filename, final String dtdFilename) throws UncheckedIOException {
-		// yyyyyy if this is a necessary/useful method, I would prefer it in the superclass.  kai, jul'16
-		
+		// yyyyyy if this is a necessary/useful method, I would prefer it in the
+		// superclass. kai, jul'16
+
 		log.info("trying to read config from " + filename);
-	  this.localDtd = dtdFilename;
+		this.localDtd = dtdFilename;
 		readFile(filename);
 		this.localDtd = null;
 	}
@@ -96,42 +105,43 @@ public final class ConfigReader extends MatsimXmlParser {
 		if (CONFIG_V1.equals(doctype)) {
 			this.delegate = new ConfigReaderMatsimV1(this.config);
 			log.info("using config_v1-reader.");
-		}
-		else if ( CONFIG_V2.equals( doctype ) ) {
-			this.delegate = new ConfigReaderMatsimV2( this.config );
-			log.info( "using config_v2-reader" );
-		}
-		else {
+		} else if (CONFIG_V2.equals(doctype)) {
+			this.delegate = new ConfigReaderMatsimV2(this.config);
+			log.info("using config_v2-reader");
+		} else {
 			throw new IllegalArgumentException("Doctype \"" + doctype + "\" not known.");
 		}
 	}
 
-	// The following did override the inherited resolveEntity method.  But I have no idea why that may have made sense.  kai, jul'16
-//	@Override
-//	public InputSource resolveEntity(final String publicId, final String systemId) {
-//		InputSource is = super.resolveEntity(publicId, systemId);
-//		if (is != null) {
-//			// everything is fine, we can access the dtd
-//			return is;
-//		}
-//		// okay, standard procedure failed... let's see if we have it locally
-//		if (this.localDtd != null) {
-//			File dtdFile = new File(this.localDtd);
-//			if (dtdFile.exists() && dtdFile.isFile() && dtdFile.canRead()) {
-//				log.info("Using the local DTD " + this.localDtd);
-//				return new InputSource(this.localDtd);
-//			}
-//		}
-//		// hmm, didn't find the local one either... maybe inside a jar somewhere?
-//		int index = systemId.replace('\\', '/').lastIndexOf('/');
-//		String shortSystemId = systemId.substring(index + 1);
-//		InputStream stream = this.getClass().getResourceAsStream("/dtd/" + shortSystemId);
-//		if (stream != null) {
-//			log.info("Using local DTD from jar-file " + shortSystemId);
-//			return new InputSource(stream);
-//		}
-//		// we fail...
-//		return null;
-//	}
+	// The following did override the inherited resolveEntity method. But I have no
+	// idea why that may have made sense. kai, jul'16
+	// @Override
+	// public InputSource resolveEntity(final String publicId, final String
+	// systemId) {
+	// InputSource is = super.resolveEntity(publicId, systemId);
+	// if (is != null) {
+	// // everything is fine, we can access the dtd
+	// return is;
+	// }
+	// // okay, standard procedure failed... let's see if we have it locally
+	// if (this.localDtd != null) {
+	// File dtdFile = new File(this.localDtd);
+	// if (dtdFile.exists() && dtdFile.isFile() && dtdFile.canRead()) {
+	// log.info("Using the local DTD " + this.localDtd);
+	// return new InputSource(this.localDtd);
+	// }
+	// }
+	// // hmm, didn't find the local one either... maybe inside a jar somewhere?
+	// int index = systemId.replace('\\', '/').lastIndexOf('/');
+	// String shortSystemId = systemId.substring(index + 1);
+	// InputStream stream = this.getClass().getResourceAsStream("/dtd/" +
+	// shortSystemId);
+	// if (stream != null) {
+	// log.info("Using local DTD from jar-file " + shortSystemId);
+	// return new InputSource(stream);
+	// }
+	// // we fail...
+	// return null;
+	// }
 
 }
