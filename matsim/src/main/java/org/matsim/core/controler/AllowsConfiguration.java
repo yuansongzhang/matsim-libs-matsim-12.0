a0edd29267e63ad2aac5f782393ba7d9ec2da4e3
Yuansong Zhang <306336548@qq.com>
2020-10-16 21:31:55 +0800

    Learn AllowConfiguration

--- matsim/src/main/java/org/matsim/core/controler/AllowsConfiguration.java
+++ matsim/src/main/java/org/matsim/core/controler/AllowsConfiguration.java
@@ -19,14 +19,21 @@
  *                                                                         *
  * *********************************************************************** */
 
- package org.matsim.core.controler;
+package org.matsim.core.controler;
 
 import org.matsim.core.mobsim.qsim.AbstractQSimModule;
 import org.matsim.core.mobsim.qsim.components.QSimComponentsConfigurator;
 
-public interface AllowsConfiguration{
-	AllowsConfiguration addOverridingModule( AbstractModule abstractModule );
-	AllowsConfiguration addOverridingQSimModule( AbstractQSimModule qsimModule );
-	AllowsConfiguration addQSimModule( AbstractQSimModule qsimModule ) ;
-	AllowsConfiguration configureQSimComponents( QSimComponentsConfigurator configurator ) ;
+/**
+ * Yuansong Zhang learning
+ */
+public interface AllowsConfiguration {
+	// Class member method
+	AllowsConfiguration addOverridingModule(AbstractModule abstractModule);
+
+	AllowsConfiguration addOverridingQSimModule(AbstractQSimModule qsimModule);
+
+	AllowsConfiguration addQSimModule(AbstractQSimModule qsimModule);
+
+	AllowsConfiguration configureQSimComponents(QSimComponentsConfigurator configurator);
 }
